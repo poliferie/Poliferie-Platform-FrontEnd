bb44d9abea7a1d2d6aae0ce39e3adf0a4110c5ce
Andrea Littera <consolelite@gmail.com>
2019-06-27 13:30:34 +0200

    Added Icons support with material-ui Basic elements for Universiry View

--- src/components/dataViewer/UniversityViewer.jsx
+++ src/components/dataViewer/UniversityViewer.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import CloseIcon from '@material-ui/icons/CancelOutlined';
 
 const useStyles = theme => ({
   card: {
@@ -23,13 +31,21 @@ const useStyles = theme => ({
   }
 });
 
+
+const CloseButton = {
+  position:'absolute',
+  top:10,
+  right:10
+};
+
+
 class UniversityViewer extends Component {
   constructor(props) {
     super(props);
     this.state = {};
 
     // Shorthand for uni id
-    this.id = this.props.match.params.id;
+    //this.id = this.props.match.params.id;
     // but I've figured out a way to directly pass elem to be shown
     // that will be enough for all our use-cases.
     this.elem = this.props.location.state;
@@ -37,38 +53,106 @@ class UniversityViewer extends Component {
 
   render() {
     const classes = useStyles();
+    const { goBack } = this.props.history;
     const bull = <span className={classes.bullet}>•</span>;
 
     return (
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography
-            className={classes.title}
-            color="textSecondary"
-            gutterBottom
+        <Card className={classes.card}>
+          <CardContent>
+            <Typography variant="h5" component="h2" style={{paddingRight:"28px"}}>
+              Università Degli studi la Sapienza
+            </Typography>
+            <Typography variant="h6" component="h3">
+              Roma
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              adjective
+            </Typography>
+            <Typography variant="body2" component="p">
+              well meaning and kindly.
+              <br />
+              {'"a benevolent smile"'}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+            <Button style={CloseButton}  onClick={() => this.props.history.goBack()}><CloseIcon fontSize="large" style={{color: "#f25e5e"}}/></Button>
+          </CardActions>
+
+          <ExpansionPanel>
+          <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel4bh-content"
+              id="panel4bh-header"
           >
-            Word of the Day
-          </Typography>
-          <Typography variant="h5" component="h2">
-            be
-            {bull}
-            nev
-            {bull}o{bull}
-            lent
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            adjective
-          </Typography>
-          <Typography variant="body2" component="p">
-            well meaning and kindly.
-            <br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
+            <Typography >
+              <Typography variant="h6">
+                Soddisfazione
+              </Typography>
+              Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
+              vitae egestas augue. Duis vel est augue. <br /> AAA </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
+              vitae egestas augue. Duis vel est augue.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+          <ExpansionPanel>
+            <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel4bh-content"
+                id="panel4bh-header"
+            >
+              <Typography >
+                <Typography variant="h6">
+                  Costi
+                </Typography>
+                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
+                vitae egestas augue. Duis vel est augue. <br /> AAA </Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>
+                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
+                vitae egestas augue. Duis vel est augue.
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+          <ExpansionPanel>
+            <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel4bh-content"
+                id="panel4bh-header"
+            >
+
+                <Typography variant="h6">
+                  Contributi </Typography>
+              <List dense={true}>
+
+                    <ListItem>
+
+                      <ListItemText
+                          primary="Single-line item"
+                          secondary={true ? 'Secondary text' : null}
+                      />
+                    </ListItem>
+              </List>
+              <Typography >
+                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
+                vitae egestas augue. Duis vel est augue. <br /> AAA </Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>
+                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
+                vitae egestas augue. Duis vel est augue.
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+
+
+
+        </Card>
     );
   }
 }
