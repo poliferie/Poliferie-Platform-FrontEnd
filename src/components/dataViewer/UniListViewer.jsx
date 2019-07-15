fccfec86a1e77ee050c9f7972c0b9f14076927e2
AdrianoPi <24545691+AdrianoPi@users.noreply.github.com>
2019-07-15 18:50:29 +0200

    Improved UI

--- src/components/dataViewer/UniListViewer.jsx
+++ src/components/dataViewer/UniListViewer.jsx
@@ -2,11 +2,36 @@ import React, { Component } from "react";
 import ListItemLink from "./ListItemLink";
 import List from "@material-ui/core/List";
 
+//ICONS
+import LocationIcon from "@material-ui/icons/LocationOn";
+import PplIcon from "@material-ui/icons/SupervisorAccount";
+import LockOpenIcon from "@material-ui/icons/LockOpen";
+import LockCloseIcon from "@material-ui/icons/Lock";
+
 class UniListViewer extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
+  lockIcon(id) {
+    if (this.props.filteredUni[id].Info.Statale === "L") {
+      return (
+        <div>
+          <LockCloseIcon style={{ marginBottom: "-0.25em" }} />
+          {"Privata"}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <LockOpenIcon style={{ marginBottom: "-0.25em" }} />
+          {"Pubblica"}
+        </div>
+      );
+    }
+  }
+
   render() {
     var filteredUni = this.props.filteredUni;
     return (
@@ -16,11 +41,33 @@ class UniListViewer extends Component {
           {Object.keys(filteredUni).map(id => (
             <ListItemLink
               key={id}
-              icon="UniIcon"
+              icon={
+                <div style={{ textAlign: "center", width: "4.5em" }}>
+                  <LocationIcon />
+
+                  <div>{filteredUni[id].Info.Regione}</div>
+                </div>
+              }
               to={{ pathname: "university/" + id }}
-              primary={JSON.stringify(filteredUni[id].Info.NomeOperativo)}
+              //primary={JSON.stringify(filteredUni[id].Info.NomeEsteso)}
+              primary={filteredUni[id].Info.NomeEsteso}
               secondary={
-                "Students: " + JSON.stringify(filteredUni[id].Info.Iscritti)
+                <div
+                  style={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                    flexDirection: "row",
+                    maxWidth: "100%"
+                  }}
+                >
+                  <div style={{ width: "6em" }}>
+                    <PplIcon style={{ marginBottom: "-0.25em" }} />
+                    {JSON.parse(filteredUni[id].Info.Iscritti)
+                      ? filteredUni[id].Info.Iscritti
+                      : "--"}
+                  </div>
+                  {this.lockIcon(id)}
+                </div>
               }
             />
           ))}
